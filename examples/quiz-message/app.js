ef80289b3432781186d644c508d18f098f9205ae
Stephen Walters <stephenwalters@gmail.com>
2010-11-11 01:15:13 +0000

    Fixed example app (it works now)
    - Also pounded out some bugs in rest-client
    - Added a response object for TwiML

--- examples/quiz-message/app.js
+++ examples/quiz-message/app.js
@@ -15,13 +15,33 @@ var twiml = require('../../lib/twiml'),
             }
         },
         {
-            q: 'What relatively common ailment is conjunctivitus?',
+            q: 'The capital city of Ontario is:',
             correct: 3,
             answers: {
-                1: 'Cold sores',
-                2: 'Carpal tunnel',
-                3: 'Pink eye',
-                4: 'Severed limb'
+                1: 'Ottawa',
+                2: 'Fonthill',
+                3: 'Toronto',
+                4: 'Windsor'
+            }
+        },
+        {
+            q: 'The capital city of Canada is:',
+            correct: 2,
+            answers: {
+                1: 'New York',
+                2: 'Ottawa',
+                3: 'Vancouver',
+                4: 'Toronto'
+            }
+        },
+        {
+            q: 'Canada\'s current prime minister is:',
+            correct: 1,
+            answers: {
+                1: 'Stephen Harper',
+                2: 'Stephen Walters',
+                3: 'Jen Chretien',
+                4: 'Margaret Atwood'
             }
         }
     ],
@@ -32,7 +52,7 @@ var twiml = require('../../lib/twiml'),
 
 twilio.addIncomingCallCallback(creds.incoming, function(req, res) {
     var intro = new twiml.Say(
-        'Welcome to Stephen\'s weird trivia game. It works like so: ' +
+        'Welcome to Stephen\'s weird Canadian trivia game. It works like so. ' +
         'You will be asked a multiple choice question. Each answer is ' +
         'preceeded by a number. Choose an answer, and press that number on ' +
         'your phone. If you answer correctly, you get to replace the ' +
@@ -41,14 +61,27 @@ twilio.addIncomingCallCallback(creds.incoming, function(req, res) {
         question = new twiml.Say(q.q),
         playMessage = currentWinnerMessage ? new twiml.Play(currentWinnerMessage) : null,
         resp = new twiml.Response();
-
+    
     resp.append(intro);
     
     if(playMessage) {
         resp.append(playMessage);
     }
     
-    var gatherUri = twilio.getGenericCallbackUri(function(req, res) {
+    function onRecord(req, res) {
+        currentWinnerMessage = req.body.RecordingUrl || 
+            currentWinnerMessage || '';
+        var congrats = new twiml.Response();
+        congrats.append(new twiml.Say('Thanks for playing!'));
+        congrats.append(new twiml.Hangup());
+        res.send(congrats.toString());
+    };
+
+    var onRecordUri = twilio.getGenericCallbackUri(onRecord);
+    
+    function onGather(req, res) {
+        console.log('onGather');
+        console.log('Digits: ' + req.body.Digits);
         if(req.body.Digits[0] == q.correct) {
             var correctResp = new twiml.Response();
             
@@ -56,17 +89,8 @@ twilio.addIncomingCallCallback(creds.incoming, function(req, res) {
                 'You may now record a message for new participants! ' + 
                 'Press pound when you are done recording.'));
             
-            var recordUri = twilio.getGenericCallbackUri(function(req, res) {
-                currentWinnerMessage = req.body.RecordingUrl || 
-                    currentWinnerMessage || '';
-                var congrats = new twiml.Response();
-                congrats.append(new twiml.Say('Thanks for playing!'));
-                congrats.append(new twiml.Hangup());
-                res.send(congrats.toString());
-            });
-            
             correctResp.append(new twiml.Record({
-                action: recordUri,
+                action: onRecordUri,
                 method: 'POST',
                 maxLength: 10,
                 transcribe: true,
@@ -74,25 +98,37 @@ twilio.addIncomingCallCallback(creds.incoming, function(req, res) {
             }));
             correctResp.append(new twiml.Say('Goodbye'));
             correctResp.append(new twiml.Hangup());
+            console.log(correctResp.toString());
+            res.send(correctResp.toString());
         } else {
+            var incorrectResp = new twiml.Response();
 
+            incorrectResp.append(new twiml.Say('You are incorrect. I hope ' +
+                'you feel really bad about this.'));
+            incorrectResp.append(new twiml.Hangup());
+            res.send(incorrectResp.toString());
         }
-    });
-    
-    var questionText = q.q;
-    var cur = 1;
+    };
+        
+    var onGatherUri = twilio.getGenericCallbackUri(onGather),
+        questionText = 'Here is your question. ' + q.q + '. ',
+        cur = 1;
+
+    console.log('Record: ' + onRecordUri + ' Gather: ' + onGatherUri);
+
     while(q.answers[cur]) {
-        questionText += key + q.answers[key];
+        questionText +=  cur + '. ' + q.answers[cur] + '. ';
         cur++;
     }
 
     resp.append(new twiml.Gather(
         new twiml.Say(questionText),
         {
-            action: gatherUri,
+            action: onGatherUri,
             method: 'POST',
             numDigits: 1
         }
     ));
+    console.log(resp.toString());
     res.send(resp.toString());
 });
