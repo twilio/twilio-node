897675855aa96c64b1e6a64ff4910648a4a1581d
Kevin Whinnery <kevin.whinnery@gmail.com>
2012-12-11 13:55:09 -0600

    changing resource names to camel case, allow either upper-cased or camel-cased parameter names in param hashes

--- spec/client.accounts.spec.js
+++ spec/client.accounts.spec.js
@@ -6,7 +6,7 @@ describe('The Twilio REST Client Accounts resource', function () {
     var client = new twilio.RestClient(config.accountSid, config.authToken);
 
     it('gets an unfiltered list of Accounts associated with this master account', function (done) {
-        client.Accounts.get(function (err, data, response) {
+        client.account.get(function (err, data, response) {
             expect(data.accounts.length).toBeGreaterThan(0);
             done();
         });
@@ -15,14 +15,14 @@ describe('The Twilio REST Client Accounts resource', function () {
     var newAccountSidOne, newAccountSidTwo; //store so we can close them after creation
 
     it('allows for the creation of subaccounts', function (done) {
-        client.Accounts.create({
-            FriendlyName:'TestAccountUno'
+        client.account.create({
+            friendlyName:'TestAccountUno'
         }, function (err, data, response) {
             expect(data.friendly_name).toBe('TestAccountUno');
             newAccountSidOne = data.sid;
 
-            //Create a second account using the "post" function
-            client.Accounts.post({
+            //Create a second account using the "post" function, and an uppercased name
+            client.account.post({
                 FriendlyName:'TestAccountDos'
             }, function (err2, data2, response2) {
                 expect(data2.friendly_name).toBe('TestAccountDos');
@@ -33,21 +33,21 @@ describe('The Twilio REST Client Accounts resource', function () {
     });
 
     it('provides a means of getting account details for a specific sid', function (done) {
-        client.Accounts(newAccountSidOne).get(function (err, data) {
+        client.account(newAccountSidOne).get(function (err, data) {
             expect(data.sid).toBe(newAccountSidOne);
             done();
         });
     });
 
     it('provdes a means of updating and closing subaccounts', function (done) {
-        client.Accounts(newAccountSidOne).put({
+        client.account(newAccountSidOne).put({
             Status:'closed'
         }, function (err, data) {
             expect(data.sid).toBe(newAccountSidOne);
             expect(data.status).toBe('closed');
 
-            client.Accounts(newAccountSidTwo).update({
-                Status:'closed'
+            client.account(newAccountSidTwo).update({
+                status:'closed'
             }, function (err2, data2) {
                 expect(data2.sid).toBe(newAccountSidTwo);
                 expect(data2.status).toBe('closed');
