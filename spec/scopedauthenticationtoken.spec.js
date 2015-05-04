41e95b78f8e1abfd33de4dd8e5e6692f38c4cba5
Alexandre Payment <apayment@twilio.com>
2015-05-04 13:41:52 -0700

    Implement scoped authentication token

--- spec/scopedauthenticationtoken.spec.js
+++ spec/scopedauthenticationtoken.spec.js
@@ -1,4 +1,5 @@
-var twilio = require('../index');
+var twilio = require('../index'),
+    jwt = require('jwt-simple');
 
 describe('The Scoped Authentication Token Object', function () {
     describe('constructor', function () {
@@ -43,6 +44,75 @@ describe('The Scoped Authentication Token Object', function () {
     });
 
     describe('addGrant', function() {
+        it('it should add a new grant to the set of grants', function() {
+            var c = new twilio.ScopedAuthenticationToken('SK123', 'AC123', 'Token1', 3600, [new twilio.Grant()]);
+            var grant = new twilio.Grant('https://taskrouter.twilio.com/**', [twilio.Grant.Action.ALL])
+            c.addGrant(grant);
 
+            expect(c.grants).toBeDefined();
+            expect(c.grants.length).toBe(2);
+            expect(c.grants[0].res).toBe('https://api.twilio.com/**');
+            expect(c.grants[0].act[0]).toBe('*');
+            expect(c.grants[1].res).toBe('https://taskrouter.twilio.com/**');
+            expect(c.grants[1].act[0]).toBe('*');
+        });
+    });
+
+    describe('generateToken', function() {
+        it('should generate a valid JWT', function() {
+            var c = new twilio.ScopedAuthenticationToken('SK123', 'AC123', 'Token1', 3600, [new twilio.Grant()]);
+
+            var token = c.generateToken('secret');
+            expect(token).toBeDefined();
+
+            var decodedToken = jwt.decode(token, 'secret');
+            expect(decodedToken).toBeDefined();
+            expect(decodedToken.jti).toBe('Token1');
+            expect(decodedToken.iss).toBe('SK123');
+            expect(decodedToken.sub).toBe('AC123');
+            expect(decodedToken.nbf).toBeDefined();
+            expect(decodedToken.exp).toBeDefined();
+            expect(decodedToken.nbf + 3600).toBe(decodedToken.exp);
+            expect(decodedToken.grants).toBeDefined();
+            expect(decodedToken.grants.length).toBe(1);
+            expect(decodedToken.grants[0].res).toBe('https://api.twilio.com/**');
+            expect(decodedToken.grants[0].act[0]).toBe('*');
+        });
+
+        it('should generate a valid JWT without an explicit grant', function() {
+            var c = new twilio.ScopedAuthenticationToken('SK123', 'AC123', 'Token1', 3600);
+
+            var token = c.generateToken('secret');
+            expect(token).toBeDefined();
+
+            var decodedToken = jwt.decode(token, 'secret');
+            expect(decodedToken).toBeDefined();
+            expect(decodedToken.jti).toBe('Token1');
+            expect(decodedToken.iss).toBe('SK123');
+            expect(decodedToken.sub).toBe('AC123');
+            expect(decodedToken.nbf).toBeDefined();
+            expect(decodedToken.exp).toBeDefined();
+            expect(decodedToken.nbf + 3600).toBe(decodedToken.exp);
+            expect(decodedToken.grants).toBeDefined();
+            expect(decodedToken.grants.length).toBe(0);
+        });
+
+        it('should generate a valid JWT without a token id', function() {
+            var c = new twilio.ScopedAuthenticationToken('SK123', 'AC123');
+
+            var token = c.generateToken('secret');
+            expect(token).toBeDefined();
+
+            var decodedToken = jwt.decode(token, 'secret');
+            expect(decodedToken).toBeDefined();
+            expect(decodedToken.jti).toBeDefined();
+            expect(decodedToken.iss).toBe('SK123');
+            expect(decodedToken.sub).toBe('AC123');
+            expect(decodedToken.nbf).toBeDefined();
+            expect(decodedToken.exp).toBeDefined();
+            expect(decodedToken.nbf + 3600).toBe(decodedToken.exp);
+            expect(decodedToken.grants).toBeDefined();
+            expect(decodedToken.grants.length).toBe(0);
+        });
     });
 });
