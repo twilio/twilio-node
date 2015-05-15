c32994fa71280e8f2bc6749f71f41027524b9ae4
matt <matt@twilio.com>
2015-05-15 01:26:17 -0400

    Add addRestGrant and enableNTS to AccessToken
    Also fixed up some tests that didn't get the update about secret
    being in the constructor

--- spec/accesstoken.spec.js
+++ spec/accesstoken.spec.js
@@ -3,33 +3,37 @@ var twilio = require('../index'),
 
 describe('The Scoped Authentication Token Object', function () {
     describe('constructor', function () {
+        it('should allow for explicit construction of a scoped authentication token with a default ttl', function() {
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
+
+            expect(c).toBeDefined();
+            expect(c.signingKeySid).toBe('SK123');
+            expect(c.accountSid).toBe('AC123');
+            expect(c.secret).toBe('secret');
+            expect(c.ttl).toBe(3600);
+            expect(c.grants).toBeDefined();
+            expect(c.grants.length).toBe(0);
+        });
+
+
         it('should allow for explicit construction of a scoped authentication token', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123', 3600);
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret', 3600);
 
             expect(c).toBeDefined();
             expect(c.signingKeySid).toBe('SK123');
             expect(c.accountSid).toBe('AC123');
+            expect(c.secret).toBe('secret');
             expect(c.ttl).toBe(3600);
             expect(c.grants).toBeDefined();
             expect(c.grants.length).toBe(0);
         });
 
-        it('should allow for explicit construction of a scoped authentication token without a grant', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123', 3600);
-
-            expect(c).toBeDefined();
-            expect(c.signingKeySid).toBe('SK123');
-            expect(c.accountSid).toBe('AC123');
-            expect(c.ttl).toBe(3600);
-            expect(c.grants).toBeDefined();
-            expect(c.grants.length).toBe(0);
-        });
 
     });
 
     describe('addGrant', function() {
         it('it should allow the user to add grants', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123', 3600);
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
             c.addGrant('https://taskrouter.twilio.com/**', twilio.AccessToken.ACTION.ALL);
             c.addGrant('https://api.twilio.com/**', twilio.AccessToken.ACTION.HTTP.GET);
 
@@ -42,7 +46,7 @@ describe('The Scoped Authentication Token Object', function () {
         });
 
         it('it should use ALL for default grant', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123', 3600);
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
             c.addGrant('https://taskrouter.twilio.com/**');
 
             expect(c.grants).toBeDefined();
@@ -54,7 +58,7 @@ describe('The Scoped Authentication Token Object', function () {
 
     describe('addEndpointGrant', function() {
         it('it should allow the user to add grants for a SIP endpoint', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123');
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
             c.addEndpointGrant('doofus');
 
             expect(c.grants).toBeDefined();
@@ -66,11 +70,35 @@ describe('The Scoped Authentication Token Object', function () {
         });
     });
 
+    describe('addRestGrant', function() {
+        it('it should allow the user to add grants for a REST endpoint', function() {
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
+            c.addRestGrant('/Apps');
+
+            expect(c.grants).toBeDefined();
+            expect(c.grants.length).toBe(1);
+            expect(c.grants[0].res).toBe('https://api.twilio.com/2010-04-01/Accounts/AC123/Apps');
+            expect(c.grants[0].act[0]).toBe('*');
+        });
+    });
+
+    describe('enableNTS', function() {
+        it('it should allow the user to enable Network Traversal Service', function() {
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
+            c.enableNTS();
+
+            expect(c.grants).toBeDefined();
+            expect(c.grants.length).toBe(1);
+            expect(c.grants[0].res).toBe('https://api.twilio.com/2010-04-01/Accounts/AC123/Tokens');
+            expect(c.grants[0].act[0]).toBe('POST');
+        });
+    });
+
     describe('toJwt', function() {
         it('should generate a valid JWT', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123', 3600);
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
 
-            var token = c.toJwt('secret');
+            var token = c.toJwt();
             expect(token).toBeDefined();
 
             var decodedToken = jwt.decode(token, 'secret');
@@ -85,9 +113,9 @@ describe('The Scoped Authentication Token Object', function () {
         });
 
         it('should generate a valid JWT without an explicit grant', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123', 3600);
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
 
-            var token = c.toJwt('secret');
+            var token = c.toJwt();
             expect(token).toBeDefined();
 
             var decodedToken = jwt.decode(token, 'secret');
@@ -102,9 +130,9 @@ describe('The Scoped Authentication Token Object', function () {
         });
 
         it('should generate a valid JWT without a token id', function() {
-            var c = new twilio.AccessToken('SK123', 'AC123');
+            var c = new twilio.AccessToken('SK123', 'AC123', 'secret');
 
-            var token = c.toJwt('secret');
+            var token = c.toJwt();
             expect(token).toBeDefined();
 
             var decodedToken = jwt.decode(token, 'secret');
