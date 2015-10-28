cc02555d960032b0fd5722c9e08f7e84dfe4e5fa
Ragil Prasetya <rprasetya@twilio.com>
2015-10-28 16:43:31 -0700

    Added identity tokens

---
+++ spec/identitytoken.spec.js
@@ -0,0 +1,175 @@
+var twilio = require('../index');
+var jwt = require('jsonwebtoken');
+
+describe('IdentityToken', function() {
+  var accountSid = 'ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+  var keySid = 'SKaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+
+  describe('constructor', function() {
+    var initWithoutIndex = function(index) {
+      return function() {
+        var constructorArgs = [accountSid, keySid, 'secret'];
+        constructorArgs[index] = undefined;
+
+        // add context
+        constructorArgs.unshift({});
+        new (Function.prototype.bind.apply(twilio.IdentityToken, constructorArgs));
+      };
+    };
+    it('should require accountSid', function() {
+      expect(initWithoutIndex(0)).toThrow(new Error('accountSid is required'));
+    });
+    it('should require keySid', function() {
+      expect(initWithoutIndex(1)).toThrow(new Error('keySid is required'));
+    });
+    it('should require secret', function() {
+      expect(initWithoutIndex(2)).toThrow(new Error('secret is required'));
+    });
+  });
+
+  describe('generate', function() {
+    it('should throw error when identity is not specified', function() {
+      var token = new twilio.IdentityToken(accountSid, keySid, 'secret');
+      expect(function() {
+        token.generate();
+      }).toThrow(new Error('identity is required'));
+    });
+
+    it('should create a token without any grants', function() {
+      var token = new twilio.IdentityToken(accountSid, keySid, 'secret');
+      token.identity = 'ID@example.com';
+
+      var decoded = jwt.verify(token.generate(), 'secret');
+      expect(decoded.jti.indexOf(keySid)).toBe(0);
+      expect(decoded.iss).toBe(keySid);
+      expect(decoded.sub).toBe(accountSid);
+      expect(decoded.exp - decoded.iat).toBe(3600);
+      expect(decoded.grants).toEqual({
+        identity: 'ID@example.com'
+      });
+    });
+
+    it('should accept user defined ttl', function() {
+      var token = new twilio.IdentityToken(accountSid, keySid, 'secret');
+      token.ttl = 100;
+      token.identity = 'ID@example.com';
+
+      var decoded = jwt.verify(token.generate(), 'secret');
+      expect(decoded.exp - decoded.iat).toBe(100);
+    });
+
+    it('should create token with ip messaging grant', function() {
+      var token = new twilio.IdentityToken(accountSid, keySid, 'secret');
+      token.identity = 'ID@example.com';
+
+      var grant = new twilio.IdentityToken.IpMessagingGrant();
+      grant.serviceSid = 'SRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+      grant.addRole('user');
+      grant.addRole('admin');
+      grant.endpointId = 'blah';
+      token.addGrant(grant);
+
+      var decoded = jwt.verify(token.generate(), 'secret');
+      expect(decoded.grants).toEqual({
+        identity: 'ID@example.com',
+        ip_messaging: {
+          service_sid: 'SRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+          role: ['user', 'admin'],
+          data: {
+            endpoint_id: 'blah'
+          }
+        }
+      });
+    });
+
+    it('should create token with rtc conversation grant', function() {
+      var token = new twilio.IdentityToken(accountSid, keySid, 'secret');
+      token.identity = 'ID@example.com';
+
+      var grant = new twilio.IdentityToken.RTCGrant();
+      grant.configurationProfileSid = 'CPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+      token.addGrant(grant);
+
+      var decoded = jwt.verify(token.generate(), 'secret');
+      expect(decoded.grants).toEqual({
+        identity: 'ID@example.com',
+        rtc: {
+          configuration_profile_sid: 'CPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        }
+      });
+    });
+
+    it('should create token with multiple grants', function() {
+      var token = new twilio.IdentityToken(accountSid, keySid, 'secret');
+      token.identity = 'ID@example.com';
+
+      var grant = new twilio.IdentityToken.IpMessagingGrant();
+      grant.serviceSid = 'SRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+      grant.addRole('user');
+      grant.addRole('admin');
+      grant.endpointId = 'blah';
+      token.addGrant(grant);
+
+      grant = new twilio.IdentityToken.RTCGrant();
+      grant.configurationProfileSid = 'CPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+      token.addGrant(grant);
+
+      var decoded = jwt.verify(token.generate(), 'secret');
+      expect(decoded.grants).toEqual({
+        identity: 'ID@example.com',
+        ip_messaging: {
+          service_sid: 'SRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+          role: ['user', 'admin'],
+          data: {
+            endpoint_id: 'blah'
+          }
+        },
+        rtc: {
+          configuration_profile_sid: 'CPaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+        }
+      });
+    });
+
+    describe('IpMessagingGrant', function() {
+      describe('toPayload', function() {
+        it('should only populate set properties', function() {
+          var grant = new twilio.IdentityToken.IpMessagingGrant();
+          expect(grant.toPayload()).toEqual({});
+
+          grant.addRole('user');
+          expect(grant.toPayload()).toEqual({
+            role: ['user']
+          });
+
+          grant.serviceSid = 'SRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+          expect(grant.toPayload()).toEqual({
+            service_sid: 'SRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            role: ['user']
+          });
+
+          grant.endpointId = 'endpointId';
+          expect(grant.toPayload()).toEqual({
+            service_sid: 'SRaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+            role: ['user'],
+            data: {
+              endpoint_id: 'endpointId'
+            }
+          });
+        });
+      });
+    });
+
+    describe('RTCGrant', function() {
+      it('should only populate set properties', function() {
+        var grant = new twilio.IdentityToken.RTCGrant();
+        expect(grant.toPayload()).toEqual({});
+
+        grant.configurationProfileSid = 'CPsid';
+        expect(grant.toPayload()).toEqual({
+          configuration_profile_sid: 'CPsid'
+        });
+      });
+    });
+  });
+
+});
