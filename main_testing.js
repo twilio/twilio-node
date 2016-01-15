693c0d9e58af9e732949ecbf3aff92537971ea9b
Ragil Prasetya <rprasetya@twilio.com>
2016-01-15 13:05:35 -0800

    Fix fetch

--- main_testing.js
+++ main_testing.js
@@ -5,11 +5,15 @@ var accountSid = process.env.TWILIO_ACCOUNT_SID;
 var token = process.env.TWILIO_AUTH_TOKEN;
 
 var twilio = new Twilio(accountSid, token);
-var promise = twilio.api.account.calls('CA123');
+console.log(twilio.api.account);
+
+var promise = twilio.api.account.fetch();
 console.log(promise);
 
-// promise.then(function(data) {
-//   console.log(data);
-// }).catch(function(error) {
-//   console.log(error);
-// }).done();
+promise.then(function(data) {
+  console.log('success');
+  console.log(data);
+}).catch(function(error) {
+  console.log('error');
+  console.log(error);
+}).done();
