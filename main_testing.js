8b625cb03211eaa8b9d3b9fa94c6c7bc131cee08
Jingming Niu <jniu@twilio.com>
2016-01-15 15:34:20 -0800

    Create working

--- main_testing.js
+++ main_testing.js
@@ -7,7 +7,11 @@ var token = process.env.TWILIO_AUTH_TOKEN;
 var twilio = new Twilio(accountSid, token);
 console.log(twilio.api.account);
 
-var promise = twilio.api.account.fetch();
+// var promise = twilio.api.account.calls('CA8db1ab523c9141a4e25f8d88e26f0fcd').fetch();
+
+var promise = twilio.trunking.trunks.create({
+    friendlyName: 'name2'
+});
 console.log(promise);
 
 promise.then(function(data) {
