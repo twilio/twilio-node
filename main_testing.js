4a8d424be6cc036dfcc8a0bb1afc8ee3104ee100
Jingming Niu <jniu@twilio.com>
2016-01-15 16:28:52 -0800

    Remove default arg in create/remove

--- main_testing.js
+++ main_testing.js
@@ -9,12 +9,12 @@ console.log(twilio.api.account);
 
 // var promise = twilio.api.account.calls('CA8db1ab523c9141a4e25f8d88e26f0fcd').fetch();
 
-// var promise = twilio.trunking.trunks.create({
-//     friendlyName: 'name2'
-// });
+var promise = twilio.trunking.trunks.create({
+    friendlyName: 'name4'
+});
 
 
-var promise = twilio.trunking.trunks('TKa321d93fceb1b1e2f816c34de1d18454').remove();
+// var promise = twilio.trunking.trunks('TKa321d93fceb1b1e2f816c34de1d18454').remove();
 console.log(promise);
 
 promise.then(function(data) {
