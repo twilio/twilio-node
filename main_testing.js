89156f723a63b1467b0fbadaf2f1ebc010edea95
Jingming Niu <jniu@twilio.com>
2016-01-20 16:25:31 -0800

    Fix jshint issues

--- main_testing.js
+++ main_testing.js
@@ -5,17 +5,17 @@ var accountSid = process.env.TWILIO_ACCOUNT_SID;
 var token = process.env.TWILIO_AUTH_TOKEN;
 
 var twilio = new Twilio(accountSid, token);
-console.log(twilio.api.account);
-
 var promise = twilio.api.account.calls.list({
     pageSize: 1
 });
 
-// var promise = twilio.trunking.trunks.list();
+//var promise = twilio.trunking.trunks.get('TK5c8d9c98df850dbf7b1066fefbcabd0c').remove();
 
 promise.then(function(data) {
   console.log('success');
-  // console.log(data);
+  console.log(_.map(data, function(call) {
+    return call.sid;
+  }));
 }).catch(function(error) {
   console.log('error');
   console.log(error);
