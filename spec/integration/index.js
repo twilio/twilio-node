687166967a73c38e407cda782d651d5de4f26e49
Ragil Prasetya <rprasetya@twilio.com>
2016-01-21 11:41:43 -0800

    Add linting to specs

---
+++ spec/integration/index.js
@@ -0,0 +1,14 @@
+var _ = require('lodash');
+var Twilio = require('lib').Twilio;
+var Holodeck = require('./holodeck');
+
+var accountSid = 'AC' + _.join(_.fill(new Array(32), 'a'), '');
+var authToken = 'AUTHTOKEN';
+
+var holodeck = new Holodeck();
+var client = Twilio(accountSid, authToken, holodeck);
+
+module.exports = {
+  holodeck: holodeck,
+  client: client
+};
