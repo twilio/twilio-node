f477cf7efb59f61847898506cc8fb14e5b798f5a
Jingming Niu <jniu@twilio.com>
2016-01-25 11:03:29 -0800

    Use omitBy instead of omit

--- main_testing.js
+++ main_testing.js
@@ -5,17 +5,17 @@ var accountSid = process.env.TWILIO_ACCOUNT_SID;
 var token = process.env.TWILIO_AUTH_TOKEN;
 
 var twilio = new Twilio(accountSid, token);
-var promise = twilio.api.account.calls.list({
-    pageSize: 1
-});
+// var promise = twilio.api.account.calls.list({
+//     pageSize: 1
+// });
 
-//var promise = twilio.trunking.trunks.get('TK5c8d9c98df850dbf7b1066fefbcabd0c').remove();
+var promise = twilio.api.account.messages.create('+18584618959', '+18589354448', {
+  body: 'hi'
+});
 
 promise.then(function(data) {
   console.log('success');
-  console.log(_.map(data, function(call) {
-    return call.sid;
-  }));
+  console.log(data)
 }).catch(function(error) {
   console.log('error');
   console.log(error);
