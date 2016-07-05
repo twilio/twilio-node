10dff017f7ca53f620e5c8481f2c6536832f8a4d
Gurudutta Ramanathaiah <gramanathaiah@twilio.com>
2016-07-05 13:36:03 -0700

    Fixing the deprecated test case.

--- spec/task_router_capability_deprecated.spec.js
+++ spec/task_router_capability_deprecated.spec.js
@@ -32,7 +32,7 @@ describe('The TaskRouter Capability Deprecated Token Object', function() {
         var token = c.generate();
 
         var decoded = jwt.decode(token, 'foobar');
-        expect(decoded['policies'].length).toBe(8);
+        expect(decoded['policies'].length).toBe(9);
         var activitiesPolicy = {
             url: 'https://taskrouter.twilio.com/v1/Workspaces/WS456/Activities',
             method: 'GET',
@@ -67,7 +67,7 @@ describe('The TaskRouter Capability Deprecated Token Object', function() {
             post_filter: {},
             allow: true
         };
-        expect(decoded['policies'][3]).toEqual(getPolicy);
+        expect(decoded['policies'][4]).toEqual(getPolicy);
 
         var postPolicy = {
             url: 'https://event-bridge.twilio.com/v1/wschannels/AC123/WK789',
@@ -76,7 +76,7 @@ describe('The TaskRouter Capability Deprecated Token Object', function() {
             post_filter: {},
             allow: true
         };
-        expect(decoded['policies'][4]).toEqual(postPolicy);
+        expect(decoded['policies'][5]).toEqual(postPolicy);
 
         var workerFetchPolicy = {
             url: 'https://taskrouter.twilio.com/v1/Workspaces/WS456/Workers/WK789',
@@ -85,7 +85,7 @@ describe('The TaskRouter Capability Deprecated Token Object', function() {
             post_filter: {},
             allow: true
         };
-        expect(decoded['policies'][5]).toEqual(workerFetchPolicy);
+        expect(decoded['policies'][6]).toEqual(workerFetchPolicy);
 
         var workerUpdatesPolicy = {
             url: 'https://taskrouter.twilio.com/v1/Workspaces/WS456/Workers/WK789',
@@ -94,7 +94,7 @@ describe('The TaskRouter Capability Deprecated Token Object', function() {
             post_filter: {'ActivitySid': {'required': true}},
             allow: true
         };
-        expect(decoded['policies'][6]).toEqual(workerUpdatesPolicy);
+        expect(decoded['policies'][7]).toEqual(workerUpdatesPolicy);
 
         var taskPolicy = {
             url: 'https://taskrouter.twilio.com/v1/Workspaces/WS456/Tasks/**',
@@ -104,6 +104,6 @@ describe('The TaskRouter Capability Deprecated Token Object', function() {
             allow: true
         };
 
-        expect(decoded['policies'][7]).toEqual(taskPolicy);
+        expect(decoded['policies'][8]).toEqual(taskPolicy);
     });
 });
