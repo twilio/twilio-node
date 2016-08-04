f2df64f9c904608bb01ca7b7971a1c2e1da0ae55
Jingming Niu <niu@jingming.ca>
2016-08-04 16:46:06 -0700

    Add programmable voice grant

--- spec/accesstoken.spec.js
+++ spec/accesstoken.spec.js
@@ -232,6 +232,29 @@ describe('AccessToken', function() {
         });
       });
     });
+
+    describe('ProgrammableVoiceGrant', function () {
+      it('should generate a grant', function() {
+        var grant = new twilio.AccessToken.ProgrammableVoiceGrant({
+          outgoingApplicationSid: 'AP123',
+          outgoingApplicationParams: {
+            foo: 'bar'
+          },
+          endpointId: 'id'
+        });
+
+        expect(grant.toPayload()).toEqual({
+          outgoing: {
+            application_sid: 'AP123',
+            params: {
+              foo: 'bar'
+            }
+          },
+          endpoint_id: 'id'
+        });
+      });
+    });
+
   });
 
 });
