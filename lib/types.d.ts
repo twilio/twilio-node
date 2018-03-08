aaac0b585f3dc416d51caf82ff9083db2dfe1015
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-08 11:27:54 +0100

    Remove types.d.ts. Not needed

--- lib/types.d.ts
+++
@@ -1,4 +0,0 @@
-import * as VoiceResponse from './twiml/VoiceResponse';
-import * as MessagingReponse from './twiml/MessagingResponse';
-import * as FaxResponse from './twiml/FaxResponse';
-export { VoiceResponse, MessagingReponse, FaxResponse };
