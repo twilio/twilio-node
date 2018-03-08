39968c070e07c7ff4809b980f3a884d764289972
Dominik Kundel <dominik.kundel@gmail.com>
2018-03-08 11:26:45 +0100

    Add basic interfaces and types

---
+++ lib/types.d.ts
@@ -0,0 +1,4 @@
+import * as VoiceResponse from './twiml/VoiceResponse';
+import * as MessagingReponse from './twiml/MessagingResponse';
+import * as FaxResponse from './twiml/FaxResponse';
+export { VoiceResponse, MessagingReponse, FaxResponse };
