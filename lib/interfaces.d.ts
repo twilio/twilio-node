23344ac4ecc610377be033d7d0d907023cad0507
Dominik Kundel <dominik.kundel@gmail.com>
2018-05-14 17:12:43 +0200

    Add generic base class for toJSON. Requires TS 2.8

--- lib/interfaces.d.ts
+++ lib/interfaces.d.ts
@@ -71,3 +71,28 @@ export interface PageOptions<TPage> {
    */
   callback?: (page: TPage) => void;
 }
+
+/**
+ * A generic type that returns all property names of a class whose type is not "function"
+ */
+export type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K;
+}[keyof T];
+
+/**
+ * A generic type that returns only properties of a class that are not functions
+ */
+export type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+
+/**
+ * A type alias for better readibility
+ */
+export type Json<T> = NonFunctionProperties<T>;
+
+export declare class SerializableClass {
+  /**
+   * Converts the current instance in a regular JSON. 
+   * It will be automatically called by JSON.stringify()
+   */
+  toJSON(): Json<this>;
+}
