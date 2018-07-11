54e33d347571aea467ac923ee1a7df7ee001400b
Twilio <twilio-ci@twilio.com>
2018-07-11 12:32:46 -0700

    add enum types to VoiceResponse

--- lib/twiml/VoiceResponse.d.ts
+++ lib/twiml/VoiceResponse.d.ts
@@ -111,6 +111,44 @@ declare class VoiceResponse {
 
 declare namespace VoiceResponse {
 
+  type ClientEvent = 'initiated'|'ringing'|'answered'|'completed';
+
+  type ConferenceBeep = 'true'|'false'|'onEnter'|'onExit';
+
+  type ConferenceEvent = 'start'|'end'|'join'|'leave'|'mute'|'hold'|'speaker';
+
+  type ConferenceRecord = 'do-not-record'|'record-from-start';
+
+  type ConferenceRecordingEvent = 'started'|'stopped'|'paused'|'resumed'|'completed'|'failed';
+
+  type ConferenceRegion = 'us1'|'ie1'|'sg1'|'br1'|'au1'|'jp1';
+
+  type ConferenceTrim = 'trim-silence'|'do-not-trim';
+
+  type DialRecord = 'do-not-record'|'record-from-answer'|'record-from-ringing'|'record-from-answer-dual'|'record-from-ringing-dual';
+
+  type DialRecordingEvent = 'in-progress'|'completed'|'failed';
+
+  type DialRingTone = 'at'|'au'|'bg'|'br'|'be'|'ch'|'cl'|'cn'|'cz'|'de'|'dk'|'ee'|'es'|'fi'|'fr'|'gr'|'hu'|'il'|'in'|'it'|'lt'|'jp'|'mx'|'my'|'nl'|'no'|'nz'|'ph'|'pl'|'pt'|'ru'|'se'|'sg'|'th'|'uk'|'us'|'us-old'|'tw'|'ve'|'za';
+
+  type DialTrim = 'trim-silence'|'do-not-trim';
+
+  type GatherInput = 'dtmf'|'speech';
+
+  type GatherLanguage = 'af-ZA'|'id-ID'|'ms-MY'|'ca-ES'|'cs-CZ'|'da-DK'|'de-DE'|'en-AU'|'en-CA'|'en-GB'|'en-IN'|'en-IE'|'en-NZ'|'en-PH'|'en-ZA'|'en-US'|'es-AR'|'es-BO'|'es-CL'|'es-CO'|'es-CR'|'es-EC'|'es-SV'|'es-ES'|'es-US'|'es-GT'|'es-HN'|'es-MX'|'es-NI'|'es-PA'|'es-PY'|'es-PE'|'es-PR'|'es-DO'|'es-UY'|'es-VE'|'eu-ES'|'il-PH'|'fr-CA'|'fr-FR'|'gl-ES'|'hr-HR'|'zu-ZA'|'is-IS'|'it-IT'|'lt-LT'|'hu-HU'|'nl-NL'|'nb-NO'|'pl-PL'|'pt-BR'|'pt-PT'|'ro-RO'|'sk-SK'|'sl-SI'|'fi-FI'|'sv-SE'|'vi-VN'|'tr-TR'|'el-GR'|'bg-BG'|'ru-RU'|'sr-RS'|'uk-UA'|'he-IL'|'ar-IL'|'ar-JO'|'ar-AE'|'ar-BH'|'ar-DZ'|'ar-SA'|'ar-IQ'|'ar-KW'|'ar-MA'|'ar-TN'|'ar-OM'|'ar-PS'|'ar-QA'|'ar-LB'|'ar-EG'|'fa-IR'|'hi-IN'|'th-TH'|'ko-KR'|'cmn-Hant-TW'|'yue-Hant-HK'|'ja-JP'|'cmn-Hans-HK'|'cmn-Hans-CN';
+
+  type NumberEvent = 'initiated'|'ringing'|'answered'|'completed';
+
+  type RecordTrim = 'trim-silence'|'do-not-trim';
+
+  type RejectReason = 'rejected'|'busy';
+
+  type SayLanguage = 'da-DK'|'de-DE'|'en-AU'|'en-CA'|'en-GB'|'en-IN'|'en-US'|'ca-ES'|'es-ES'|'es-MX'|'fi-FI'|'fr-CA'|'fr-FR'|'it-IT'|'ja-JP'|'ko-KR'|'nb-NO'|'nl-NL'|'pl-PL'|'pt-BR'|'pt-PT'|'ru-RU'|'sv-SE'|'zh-CN'|'zh-HK'|'zh-TW';
+
+  type SayVoice = 'man'|'woman'|'alice';
+
+  type SipEvent = 'initiated'|'ringing'|'answered'|'completed';
+
   /**
    * Options to pass to dial
    *
@@ -134,14 +172,14 @@ declare namespace VoiceResponse {
     callerId?: string;
     hangupOnStar?: boolean;
     method?: string;
-    record?: dial.record;
+    record?: DialRecord;
     recordingStatusCallback?: string;
-    recordingStatusCallbackEvent?: dial.recording_event;
+    recordingStatusCallbackEvent?: DialRecordingEvent;
     recordingStatusCallbackMethod?: string;
-    ringTone?: dial.ring_tone;
+    ringTone?: DialRingTone;
     timeLimit?: number;
     timeout?: number;
-    trim?: dial.trim;
+    trim?: DialTrim;
   }
 
   /**
@@ -184,8 +222,8 @@ declare namespace VoiceResponse {
     bargeIn?: boolean;
     finishOnKey?: string;
     hints?: string;
-    input?: gather.input;
-    language?: gather.language;
+    input?: GatherInput;
+    language?: GatherLanguage;
     maxSpeechTime?: number;
     method?: string;
     numDigits?: number;
@@ -257,7 +295,7 @@ declare namespace VoiceResponse {
     timeout?: number;
     transcribe?: boolean;
     transcribeCallback?: string;
-    trim?: record.trim;
+    trim?: RecordTrim;
   }
 
   /**
@@ -275,7 +313,7 @@ declare namespace VoiceResponse {
    * @property reason - Rejection reason
    */
   export interface RejectAttributes {
-    reason?: reject.reason;
+    reason?: RejectReason;
   }
 
   /**
@@ -286,9 +324,9 @@ declare namespace VoiceResponse {
    * @property language - Message langauge
    */
   export interface SayAttributes {
-    language?: say.language;
+    language?: SayLanguage;
     loop?: number;
-    voice?: say.voice;
+    voice?: SayVoice;
   }
 
   /**
@@ -316,9 +354,9 @@ declare namespace VoiceResponse {
    * @property language - Message langauge
    */
   export interface SayAttributes {
-    language?: say.language;
+    language?: SayLanguage;
     loop?: number;
-    voice?: say.voice;
+    voice?: SayVoice;
   }
 
   /**
@@ -364,7 +402,7 @@ declare namespace VoiceResponse {
   export interface ClientAttributes {
     method?: string;
     statusCallback?: string;
-    statusCallbackEvent?: client.event;
+    statusCallbackEvent?: ClientEvent;
     statusCallbackMethod?: string;
     url?: string;
   }
@@ -392,21 +430,21 @@ declare namespace VoiceResponse {
    * @property eventCallbackUrl - Event callback URL
    */
   export interface ConferenceAttributes {
-    beep?: conference.beep;
+    beep?: ConferenceBeep;
     endConferenceOnExit?: boolean;
     eventCallbackUrl?: string;
     maxParticipants?: number;
     muted?: boolean;
-    record?: conference.record;
+    record?: ConferenceRecord;
     recordingStatusCallback?: string;
-    recordingStatusCallbackEvent?: conference.recording_event;
+    recordingStatusCallbackEvent?: ConferenceRecordingEvent;
     recordingStatusCallbackMethod?: string;
-    region?: conference.region;
+    region?: ConferenceRegion;
     startConferenceOnEnter?: boolean;
     statusCallback?: string;
-    statusCallbackEvent?: conference.event;
+    statusCallbackEvent?: ConferenceEvent;
     statusCallbackMethod?: string;
-    trim?: conference.trim;
+    trim?: ConferenceTrim;
     waitMethod?: string;
     waitUrl?: string;
     whisper?: string;
@@ -426,7 +464,7 @@ declare namespace VoiceResponse {
     method?: string;
     sendDigits?: string;
     statusCallback?: string;
-    statusCallbackEvent?: number.event;
+    statusCallbackEvent?: NumberEvent;
     statusCallbackMethod?: string;
     url?: string;
   }
@@ -461,7 +499,7 @@ declare namespace VoiceResponse {
     method?: string;
     password?: string;
     statusCallback?: string;
-    statusCallbackEvent?: sip.event;
+    statusCallbackEvent?: SipEvent;
     statusCallbackMethod?: string;
     url?: string;
     username?: string;
