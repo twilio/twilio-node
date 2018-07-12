aa56ed9c162725772ff611ff4b84c591b3611c34
cjcodes <chris@cjcodes.com>
2018-07-12 14:45:36 -0400

    Add a request body validator (#365)

--- spec/validation.spec.js
+++ spec/validation.spec.js
@@ -1,206 +1,215 @@
 'use strict';
 
-var twilio = require('../lib'),
-    express = require('express'),
-    request = require('request'),
-    http = require('http'),
-    crypto = require('crypto'),
-    f = require('util').format;
+const {
+    validateRequest,
+    validateRequestWithBody,
+    validateBody,
+    webhook,
+} = require('../lib/webhooks/webhooks');
+const httpMocks = require('node-mocks-http');
+const url = require('url');
 
-var twimlString = '<?xml version="1.0" encoding="UTF-8"?><Response><Message>hi</Message></Response>',
-    params = {
-        To:'+16515556677',
-        From:'+16515556699',
-        Body:'hello áçčëñtš 😃'
-    },
-    fakeToken = 'abcdefghijklmnopqrstuvwxyz1234567890';
+const cloneObject = require('lodash/clone')
 
-// Use this for testing the various manual configurations
-// Still use well-known sig to test the signature process its self
-function createTestSig(signUrl) {
-    Object.keys(params).sort().forEach(function(key, i) {
-        signUrl = signUrl + key + params[key];
+const defaultParams = {
+    CallSid: 'CA1234567890ABCDE',
+    Caller: '+14158675309',
+    Digits: '1234',
+    From: '+14158675309',
+    To: '+18005551212',
+};
+const token = '12345';
+const defaultSignature = 'RSOYDt4T1cUTdK1PDd93/VVr8B8=';
+const requestUrl = 'https://mycompany.com/myapp.php?foo=1&bar=2';
+const body = "{\"property\": \"value\", \"boolean\": true}";
+const bodySignature = "Ch/3Y02as7ldtcmi3+lBbkFQKyg6gMfPGWMmMvluZiA=";
+const requestUrlWithHash = requestUrl + '&bodySHA256=' + bodySignature.replace('+', '%2B').replace('=', '%3D');
+const requestUrlWithHashSignature = 'afcFvPLPYT8mg/JyIVkdnqQKa2s=';
+
+describe('Request validation', () => {
+    it('should succeed with the correct signature', () => {
+        const isValid = validateRequest(token, defaultSignature, requestUrl, defaultParams);
+
+        expect(isValid).toBeTruthy();
     });
-    return crypto.createHmac('sha1', fakeToken)
-                        .update(new Buffer(signUrl, 'utf-8'))
-                        .digest('Base64');
-}
 
-describe('Testing Express request validation', function() {
-    // create a local express app
-    var app = require('express')();
+    it('should fail when given the wrong signature', () => {
+        const isValid = validateRequest(token, 'WRONG_SIGNATURE', requestUrl, defaultParams);
 
-    // Use url-encoded body parser
-    app.use(express.urlencoded({ extended: false }));
+        expect(isValid).toBeFalsy();
+    });
 
-    // create a simple TwiML-serving web app that will validate a request
-    // was originated by Twilio
-    var requestValidated = false;
-    var twiml = new twilio.twiml.MessagingResponse();
-    twiml.message('hi');
+    it('should validate post body correctly', () => {
+        const isValid = validateBody(body, bodySignature);
 
-    app.post('/sms', function(request, response) {
-        // Use manually
-        if (twilio.validateExpressRequest(request, fakeToken)) {
-            requestValidated = true;
-            response.type('text/xml');
-            response.send(twiml.toString());
-        } else {
-            response
-                .type('text/plain')
-                .status(403)
-                .send('You are not Twilio >:/');
+        expect(isValid).toBeTruthy();
+    });
+
+    it('should fail to validate with wrong sha', () => {
+        const isValid = validateBody(body, 'WRONG_HASH');
+
+        expect(isValid).toBeFalsy();
+    });
+
+    it('should validate request body when given', () => {
+        const isValid = validateRequestWithBody(token, requestUrlWithHashSignature, requestUrlWithHash, body);
+
+        expect(isValid).toBeTruthy();
+    });
+
+    it('should validate request body with only sha param', () => {
+        const shortUrl = 'https://mycompany.com/myapp.php?bodySHA256=' + bodySignature.replace('+', '%2B').replace('=', '%3D');
+        const isValid = validateRequestWithBody(token, 'DXnNFCj8DJ/hZmiSg4UzaDHw5Og=', shortUrl, body);
+
+        expect(isValid).toBeTruthy();
+    });
+
+    it('should fail validation if given body but no bodySHA256 param', () => {
+        const isValid = validateRequestWithBody(token, defaultSignature, requestUrl, defaultParams, body);
+
+        expect(isValid).toBeFalsy();
+    });
+});
+
+describe('Request validation middleware', () => {
+    const fullUrl = new url.URL(requestUrl);
+    const defaultRequest = {
+        method: 'POST',
+        protocol: fullUrl.protocol,
+        host: fullUrl.host,
+        headers: {
+            'X-Twilio-Signature': defaultSignature,
+            'host': fullUrl.host,
+        },
+        url: fullUrl.pathname + fullUrl.search,
+        originalUrl: fullUrl.pathname + fullUrl.search,
+        body: defaultParams,
+    };
+    const middleware = webhook(token);
+    let response;
+
+    beforeEach(() => {
+        response = httpMocks.createResponse();
+    });
+
+    it('should validate standard requests', done => {
+        const request = httpMocks.createRequest(defaultRequest);
+
+        middleware(request, response, error => {
+            // This test will only pass if the middleware calls next().
+            done();
+        });
+
+        expect(response.statusCode).toEqual(200);
+    });
+
+    it('should send 403 for invalid signatures', () => {
+        const newUrl = fullUrl.pathname + fullUrl.search + '&somethingUnexpected=true';
+        const request = httpMocks.createRequest(Object.assign({}, defaultRequest, {
+            originalUrl: newUrl,
+        }));
+
+        middleware(request, response, error => {
+            expect(true).toBeFalsy();
+        });
+
+        expect(response.statusCode).toEqual(403);
+    });
+
+    it('should bypass validation if given {validate:false}', done => {
+        const request = httpMocks.createRequest(defaultRequest);
+
+        const middleware = webhook(token, {
+            validate: false,
+        });
+
+        middleware(request, response, error => {
+            done();
+        });
+
+        expect(response.statusCode).toEqual(200);
+    });
+
+    it('should accept manual host+proto', done => {
+        const request = httpMocks.createRequest(Object.assign({}, defaultRequest, {
+            host: 'someothercompany.com',
+            protocol: 'http',
+            headers: Object.assign({}, defaultRequest.headers, {
+                host: 'someothercompany.com',
+            }),
+        }));
+
+        const middleware = webhook(token, {
+            host: 'mycompany.com',
+            protocol: 'https',
+        });
+
+        middleware(request, response, error => {
+            done();
+        });
+
+        expect(response.statusCode).toEqual(200);
+    });
+
+    it('should accept manual url and override host+proto', done => {
+        const request = httpMocks.createRequest(Object.assign({}, defaultRequest, {
+            host: 'someothercompany.com',
+            protocol: 'http',
+            headers: Object.assign({}, defaultRequest.headers, {
+                host: 'someothercompany.com',
+            }),
+        }));
+
+        const middleware = webhook(token, {
+            host: 'myothercompany.com',
+            protocol: 'ftp',
+            url: requestUrl,
+        });
+
+        middleware(request, response, error => {
+            done();
+        });
+
+        expect(response.statusCode).toEqual(200);
+
+        if (response.statusCode !== 200) {
+            done();
         }
     });
 
-    // Create a second URL that will test the behavior of the validation
-    // middleware and TwiML-aware response monkey patch
-    app.post('/middleware', twilio.webhook(fakeToken), function(request, response) {
-        response.send(twiml.toString());
-    });
+    it('should validate post body if given a query param', done => {
+        const request = httpMocks.createRequest(Object.assign({}, defaultRequest, {
+            originalUrl: requestUrlWithHash.substring(requestUrlWithHash.indexOf('.com/') + 4),
+            body,
+            headers: Object.assign({}, defaultRequest.headers, {
+                'X-Twilio-Signature': requestUrlWithHashSignature,
+            }),
+        }));
 
-    // Just decorate the request
-    app.post('/noauth', twilio.webhook(fakeToken, {
-        validate:false
-    }), function(request, response) {
-        response.send(twiml.toString());
-    });
-
-    // Manually configure the host and protocol
-    app.post('/manual/host', twilio.webhook(fakeToken, {
-        host:'foo.twilio.com',
-        protocol:'https'
-    }), function(request, response) {
-        response.send(twiml.toString());
-    });
-
-    // Manually configure the entire URL, ignore other input
-    app.post('/manual/url', twilio.webhook(fakeToken, {
-        url:'https://bar.twilio.com/sms',
-        host:'foo.twilio.com',
-        protocol:'http'
-    }), function(request, response) {
-        response.send(twiml.toString());
-    });
-
-    //start server
-    var server = http.createServer(app);
-    server.listen(3000);
-
-    // test ngrok-exposed URL
-    var testUrl = 'http://localhost:3000';
-    it('should give a 403 from a direct POST', function(done) {
-        request({
-            method:'POST',
-            url:testUrl+'/sms'
-        }, function(err, response, body) {
-            expect(response.statusCode).toBe(403);
-            done();
-        });
-    });
-
-    it('should act okay if the request is signed the Twilio way', function(done) {
-        // Manually create a Twilio signature
-        var signUrl = testUrl+'/sms';
-        var testSig = 'FUjfbzy5aqpVEYoCcThCAKmctVo=';
-
-        // Hit our webhook with a "Twilio signed" request
-        request({
-            method:'POST',
-            url:testUrl+'/sms',
-            headers: {
-                'X-Twilio-Signature':testSig
-            },
-            form: params
-        }, function(err, response) {
-            expect(err).toBeFalsy();
-            expect(response.body).toBe(twimlString);
+        middleware(request, response, error => {
             done();
         });
 
+        expect(response.statusCode).toEqual(200);
+
+        if (response.statusCode !== 200) {
+            done();
+        }
     });
 
-    it('should render twiml with no auth', function(done) {
-        request({
-            method:'POST',
-            url:testUrl+'/noauth'
-        }, function(err, response, body) {
-            expect(response.body).toBe(twimlString);
-            done();
-        });
-    });
+    it('should fail validation of post body with wrong hash', () => {
+        const request = httpMocks.createRequest(Object.assign({}, defaultRequest, {
+            originalUrl: requestUrlWithHash.substring(requestUrlWithHash.indexOf('.com/') + 4).replace('Ch', 'Zh'),
+            body,
+            headers: Object.assign({}, defaultRequest.headers, {
+                'X-Twilio-Signature': requestUrlWithHashSignature,
+            }),
+        }));
 
-    it('should give a 403 from a direct POST', function(done) {
-        request({
-            method:'POST',
-            url:testUrl+'/middleware'
-        }, function(err, response, body) {
-            expect(response.statusCode).toBe(403);
-            done();
-        });
-    });
-
-    it('should validate with middleware', function(done) {
-        // Manually create a Twilio signature
-        var signUrl = testUrl+'/middleware';
-        var testSig = 'Eh17e3p7Yu48aMIa+yHk++1+L5s=';
-
-        // Hit our webhook with a "Twilio signed" request
-        request({
-            method:'POST',
-            url:testUrl+'/middleware',
-            headers: {
-                'X-Twilio-Signature':testSig
-            },
-            form: params
-        }, function(err, response) {
-            expect(err).toBeFalsy();
-            expect(response.body).toBe(twimlString);
-            done();
+        middleware(request, response, error => {
+            expect(true).toBeFalsy();
         });
 
-    });
-
-    it('should validate with a custom host and protocol', function(done) {
-        // Manually create a Twilio signature
-        var signUrl = testUrl+'/manual/host';
-        var testSig = createTestSig('https://foo.twilio.com/manual/host');
-
-        // Hit our webhook with a "Twilio signed" request
-        request({
-            method:'POST',
-            url:signUrl,
-            headers: {
-                'X-Twilio-Signature':testSig
-            },
-            form: params
-        }, function(err, response) {
-            expect(err).toBeFalsy();
-            expect(response.body).toBe(twimlString);
-            done();
-        });
-
-    });
-
-    it('should validate with a fully custom URL', function(done) {
-        // Manually create a Twilio signature
-        var signUrl = testUrl+'/manual/url';
-        var testSig = createTestSig('https://bar.twilio.com/sms');
-
-        // Hit our webhook with a "Twilio signed" request
-        request({
-            method:'POST',
-            url:signUrl,
-            headers: {
-                'X-Twilio-Signature':testSig
-            },
-            form: params
-        }, function(err, response) {
-            expect(err).toBeFalsy();
-            expect(response.body).toBe(twimlString);
-            server.close();
-            done();
-        });
-
+        expect(response.statusCode).toEqual(403);
     });
 });
