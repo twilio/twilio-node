05a3af0978377dbfd73b41fe447ebd5321c494ff
Twilio <twilio-ci@twilio.com>
2018-08-03 14:58:42 -0700

    fix missing imports for v2010

--- lib/rest/api/V2010.d.ts
+++ lib/rest/api/V2010.d.ts
@@ -10,6 +10,29 @@ import Version = require('../../base/Version');
 import { AccountContext } from './v2010/account';
 import { AccountList } from './v2010/account';
 import { AccountListInstance } from './v2010/account';
+import { AddressListInstance } from './v2010/account/address';
+import { ApplicationListInstance } from './v2010/account/application';
+import { AuthorizedConnectAppListInstance } from './v2010/account/authorizedConnectApp';
+import { AvailablePhoneNumberCountryListInstance } from './v2010/account/availablePhoneNumber';
+import { CallListInstance } from './v2010/account/call';
+import { ConferenceListInstance } from './v2010/account/conference';
+import { ConnectAppListInstance } from './v2010/account/connectApp';
+import { IncomingPhoneNumberListInstance } from './v2010/account/incomingPhoneNumber';
+import { KeyListInstance } from './v2010/account/key';
+import { MessageListInstance } from './v2010/account/message';
+import { NewKeyListInstance } from './v2010/account/newKey';
+import { NewSigningKeyListInstance } from './v2010/account/newSigningKey';
+import { NotificationListInstance } from './v2010/account/notification';
+import { OutgoingCallerIdListInstance } from './v2010/account/outgoingCallerId';
+import { QueueListInstance } from './v2010/account/queue';
+import { RecordingListInstance } from './v2010/account/recording';
+import { ShortCodeListInstance } from './v2010/account/shortCode';
+import { SigningKeyListInstance } from './v2010/account/signingKey';
+import { SipListInstance } from './v2010/account/sip';
+import { TokenListInstance } from './v2010/account/token';
+import { TranscriptionListInstance } from './v2010/account/transcription';
+import { UsageListInstance } from './v2010/account/usage';
+import { ValidationRequestListInstance } from './v2010/account/validationRequest';
 
 
 declare class V2010 extends Version {
