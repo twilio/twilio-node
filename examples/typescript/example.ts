a992abc6e7c81844abc7fd9851d1ed1e97291b4a
ekarson <39202283+ekarson@users.noreply.github.com>
2018-08-23 08:56:09 -0700

    Tests for typescript (#372)
    * Add base types
    
    * Add basic interfaces and types
    
    * Remove types.d.ts. Not needed
    
    * Remove unnecessary testing code
    
    * Add HTTP request/response types
    
    * Add webhook typings
    
    * Add JWT generation typings
    
    * Add typings entry file
    
    * Add example file and test command. Not in use yet
    
    * Remove typings from JS style checking
    
    * Add generic base class for toJSON. Requires TS 2.8
    
    * regenerate javascript files; add generated typescript declarations
    
    * actually add typescript declarations
    
    * update package-lock; include typescript tests in test target
    
    * remove obsolete generated files
    
    * nested options params
    
    * sort property docstrings for options interfaces
    
    * add test for namespaced attribute
    
    * fix declarations for namespaced attributes
    
    * no camel casing for namespaced attributes
    
    * do not camel case attributes with dashes
    
    * enums in resources
    
    * fix enum types in resource instance properties
    
    * fix context dependent types
    
    * update MessageListInstanceCreateOptions type in example.ts
    
    * add some missing types to example script
    
    * fix callback type for resource create methods
    
    * fix compile errors in example.ts
    
    * version dependents are list instances, not lists
    
    * regenerate with new api-defs
    
    * import list instance *and* list class into version declaration
    
    * fix missing imports for v2010
    
    * remove example with callback as first param
    
    * fix types of context properties for resources
    
    * fix some compile errors in example script; regenerate
    
    * use 'object' for empty attributes params
    
    * alphabetize / dedupe TS classes
    
    * clean up unneeded imports and type ascriptions

---
+++ examples/typescript/example.ts
@@ -0,0 +1,132 @@
+import * as _ from 'lodash';
+import twilio = require('../../');
+import {MessageListInstanceCreateOptions} from '../../lib/rest/api/v2010/account/message';
+
+const accountSid: string = process.env.TWILIO_ACCOUNT_SID || '';
+const token: string = process.env.TWILIO_AUTH_TOKEN || '';
+
+const client = twilio(accountSid, token);
+
+let i: number = 0;
+client.calls.each({
+  pageSize: 7,
+  callback: (call, done) => {
+    console.log(call.sid);
+    i++;
+    if (i === 10) {
+      done();
+    }
+  },
+  done: (err: Error) => {
+    console.log('je suis fini');
+    console.log(err);
+  }
+});
+
+client.calls.each({}, call => {
+  console.log(call.sid);
+});
+
+const from = process.env.FROM_NUMBER || '';
+const to = process.env.TO_NUMBER || '';
+
+const msgData: MessageListInstanceCreateOptions = {
+  from,
+  to,
+  body: 'create using callback'
+};
+
+// Send message using callback
+client.messages.create(msgData, (err, result) => {
+  console.log('Created message using callback');
+  console.log(result.sid);
+});
+
+// Send message using promise
+const promise = client.messages.create({
+  from: from,
+  to: to,
+  body: 'create using promises'
+});
+promise.then(message => {
+  console.log('Created message using promises');
+  console.log(message.sid);
+});
+
+// Create sip trunk
+client.trunking.v1.trunks.create(
+  {
+    friendlyName: 'sip trunking'
+  },
+  (err, result) => {
+    console.log('Created trunk with friendly name');
+    console.log(result.sid);
+    console.log(result.friendlyName);
+  }
+);
+
+const promiseTrunk = client.trunking.v1.trunks.create({
+  friendlyName: 'promise trunking'
+});
+promiseTrunk.then(trunk => {
+  console.log('Created trunk with friendly name and promises');
+  console.log(trunk.sid);
+  console.log(trunk.friendlyName);
+});
+
+const trunkSid = 'TK7e37e59861c14bb80dde245cfaad5522';
+
+// Fetch trunk sid using callback
+client.trunking.v1.trunks(trunkSid).fetch((err, result) => {
+  console.log('Fetch trunk using callback');
+  console.log(result.sid);
+});
+
+// Fetch trunk using promise
+const promiseTrunk2 = client.trunking.v1.trunks(trunkSid).fetch();
+promiseTrunk2.then(trunk => {
+  console.log('Fetch trunk using promise');
+  console.log(trunk.sid);
+});
+
+// Update trunk using callback
+client.trunking.v1.trunks(trunkSid).update(
+  {
+    friendlyName: 'callback trunk'
+  },
+  (err, result) => {
+    console.log('Updated using callbacks');
+    console.log(result.sid);
+    console.log(result.friendlyName);
+  }
+);
+
+// Update trunk using promise
+const promiseTrunk3 = client.trunking.v1.trunks(trunkSid).update({
+  friendlyName: 'promise trunk'
+});
+promiseTrunk3.then(trunk => {
+  console.log('Updated trunk with friendly name and promises');
+  console.log(trunk.sid);
+  console.log(trunk.friendlyName);
+});
+
+// List messages using callbacks
+client.messages.list({}, (err, messages) => {
+  console.log('Listing messages using callbacks');
+  _.each(messages, message => {
+    console.log(message.sid);
+  });
+});
+
+// List messages using promises
+const promiseMessage = client.messages.list();
+promiseMessage.then(messages => {
+  console.log('Listing messages using promises');
+  _.each(messages, function(message) {
+    console.log(message.sid);
+  });
+});
+
+const twiml = new twilio.twiml.VoiceResponse();
+twiml.dial({}, '+12345678901');
