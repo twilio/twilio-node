aea1df11f2e266855ba30bf48f2ae5fb0562d79c
Mark Niehe <mniehe@gmail.com>
2019-05-16 13:15:11 -0700

    Removed duplicate test

--- spec/validation.spec.js
+++ spec/validation.spec.js
@@ -133,26 +133,6 @@ describe('Request validation middleware', () => {
         expect(response.statusCode).toEqual(403);
     });
 
-    it('should send 403 when no twilio signature header', () => {
-        const newUrl = fullUrl.pathname + fullUrl.search + '&somethingUnexpected=true';
-
-        // Make a copy of the default request so the header object can be modified without
-        // affecting other tests.
-        const noSignatureRequest = Object.assign({}, defaultRequest);
-        noSignatureRequest.headers = Object.assign({}, defaultRequest.headers);
-        delete noSignatureRequest.headers['X-Twilio-Signature'];
-
-        const request = httpMocks.createRequest(Object.assign({}, noSignatureRequest, {
-            originalUrl: newUrl,
-        }));
-
-        middleware(request, response, error => {
-            expect(true).toBeFalsy();
-        });
-
-        expect(response.statusCode).toEqual(403);
-    });
-
     it('should bypass validation if given {validate:false}', done => {
         const request = httpMocks.createRequest(defaultRequest);
 
