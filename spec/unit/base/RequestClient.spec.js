10cd1474018027a7a758ec28dac2e33d0e9ce7f8
Barry <barry.barrette@libertymutual.com>
2019-09-25 17:11:39 -0400

    Added the ability for users to specify a CA bundle for HTTPS requests

--- spec/unit/base/RequestClient.spec.js
+++ spec/unit/base/RequestClient.spec.js
@@ -1,3 +1,4 @@
+var mockfs = require('mock-fs');
 var proxyquire = require('proxyquire');
 
 describe('lastResponse and lastRequest defined', function() {
@@ -84,4 +85,49 @@ describe('lastRequest defined, lastResponse undefined', function() {
     expect(client.lastResponse).toBeUndefined();
   });
 
+});
+
+describe('User specified CA bundle', function() {
+  var client;
+  beforeEach(function() {
+    RequestClientMock = proxyquire('../../../lib/base/RequestClient', {
+      request: function (options, callback) {
+        callback('failed', null);
+      },
+    });
+
+    client = new RequestClientMock();
+
+    options = {
+      method: 'GET',
+      uri: 'test-uri',
+      username: 'test-username',
+      password: 'test-password',
+      headers: {'test-header-key': 'test-header-value'},
+      params: {'test-param-key': 'test-param-value'},
+      data: {'test-data-key': 'test-data-value'}
+    };
+
+    mockfs({
+      '/path/to/ca': {
+        'test-ca.pem': 'test ca data'
+      }
+    });
+  });
+
+  afterEach(function () {
+    mockfs.restore();
+  });
+
+  it('should not modify CA if not specified', function() {
+    client.request(options);
+    expect(client.lastRequest.ca).toBeUndefined();
+  });
+
+  it('should use CA if it is specified', function() {
+    process.env.TWILIO_CA_BUNDLE = '/path/to/ca/test-ca.pem';
+    client.request(options);
+    expect(client.lastRequest.ca.toString()).toEqual('test ca data');
+    delete process.env.TWILIO_CA_BUNDLE;
+  });
 });
