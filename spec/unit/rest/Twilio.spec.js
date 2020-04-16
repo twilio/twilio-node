31d93ce3ee5be641b411a660b823b2e6c115e370
Elmer Thomas <elmer@thinkingserious.com>
2020-04-15 18:13:20 -0700

    feat: add regional support

---
+++ spec/unit/rest/Twilio.spec.js
@@ -0,0 +1,51 @@
+var proxyquire = require('proxyquire');
+var Q = require('q');
+
+describe('hostname', function() {
+  var client;
+  beforeEach(function() {
+    RequestClientMock = proxyquire('../../../lib', {
+      axios: function(options) {
+        var deferred = Q.defer();
+        deferred.resolve({status: 200, data: 'voltron'});
+        return deferred.promise;
+      },
+    });
+
+    client = new RequestClientMock();
+  });
+
+  it('should use the default region if only edge is defined', function() {
+    client.edge = 'edge';
+    client.messages.create({
+      body: 'Hello from Node',
+      to: '+XXXXXXXXXX',
+      from: '+XXXXXXXXXX',
+    });
+    var uri = new URL(client._httpClient.lastRequest.url);
+    expect(uri.hostname).toEqual('api.edge.us1.twilio.com');
+  });
+
+  it('should set the region properly', function() {
+    client.region = 'region';
+    client.messages.create({
+      body: 'Hello from Node',
+      to: '+XXXXXXXXXX',
+      from: '+XXXXXXXXXX',
+    });
+    var uri = new URL(client._httpClient.lastRequest.url);
+    expect(uri.hostname).toEqual('api.region.twilio.com');
+  });
+
+  it('should set the region and edge properly', function() {
+    client.edge = 'edge';
+    client.region = 'region';
+    client.messages.create({
+      body: 'Hello from Node',
+      to: '+XXXXXXXXXX',
+      from: '+XXXXXXXXXX',
+    });
+    var uri = new URL(client._httpClient.lastRequest.url);
+    expect(uri.hostname).toEqual('api.edge.region.twilio.com');
+  });
+});
