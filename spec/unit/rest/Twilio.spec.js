7e348f981c30af10811491e7356580f134c283d3
Elmer Thomas <elmer@thinkingserious.com>
2020-04-16 16:40:28 -0700

    update tests to use nock

--- spec/unit/rest/Twilio.spec.js
+++ spec/unit/rest/Twilio.spec.js
@@ -1,82 +1,59 @@
-var proxyquire = require('proxyquire');
+'use strict';
+const nock = require('nock');
 var url = require('url');  /* jshint ignore:line */
-var Q = require('q');
 
-describe('hostname', function() {
+describe('client', () => {
   var client;
-  beforeEach(function() {
-    RequestClientMock = proxyquire('../../../lib', {
-      axios: function(options) {
-        var deferred = Q.defer();
-        deferred.resolve({status: 200, data: 'voltron'});
-        return deferred.promise;
-      },
-    });
+
+  beforeEach(() => {
+    var twilio = require('../../../lib');
+    client = new twilio('ACXXXXXXXX', 'test-password');
+    nock('https://api.twilio.com')
+      .get('/')
+      .reply(200, 'test response');
   });
 
-  it('should use the default region if only edge is defined', function() {
-    client = new RequestClientMock('ACXXXXXXXX', 'test-password');
-    client.edge = 'edge';
-    client.messages.create({
-      body: 'Hello from Node',
-      to: '+XXXXXXXXXX',
-      from: '+XXXXXXXXXX',
+  describe('setting the region', () => {
+    it('should use the default region if only edge is defined', () => {
+      client.edge = 'edge';
+      client.messages.create({
+        body: 'Hello from Node',
+        to: '+XXXXXXXXXX',
+        from: '+XXXXXXXXXX',
+      });
+      var uri = new url.URL(client._httpClient.lastRequest.url);
+      expect(uri.hostname).toEqual('api.edge.us1.twilio.com');
     });
-    var uri = new url.URL(client._httpClient.lastRequest.url);
-    expect(uri.hostname).toEqual('api.edge.us1.twilio.com');
-  });
-
-  it('should set the region properly', function() {
-    client = new RequestClientMock('ACXXXXXXXX', 'test-password');
-    client.region = 'region';
-    client.messages.create({
-      body: 'Hello from Node',
-      to: '+XXXXXXXXXX',
-      from: '+XXXXXXXXXX',
+    it('should set the region properly if only the region is specified', () => {
+      client.region = 'region';
+      client.messages.create({
+        body: 'Hello from Node',
+        to: '+XXXXXXXXXX',
+        from: '+XXXXXXXXXX',
+      });
+      var uri = new url.URL(client._httpClient.lastRequest.url);
+      expect(uri.hostname).toEqual('api.region.twilio.com');
     });
-    var uri = new url.URL(client._httpClient.lastRequest.url);
-    expect(uri.hostname).toEqual('api.region.twilio.com');
-  });
-
-  it('should set the region and edge properly', function() {
-    client = new RequestClientMock('ACXXXXXXXX', 'test-password');
-    client.edge = 'edge';
-    client.region = 'region';
-    client.messages.create({
-      body: 'Hello from Node',
-      to: '+XXXXXXXXXX',
-      from: '+XXXXXXXXXX',
+    it('should use the default region if only edge is defined', () => {
+      client.edge = 'edge';
+      client.messages.create({
+        body: 'Hello from Node',
+        to: '+XXXXXXXXXX',
+        from: '+XXXXXXXXXX',
+      });
+      var uri = new url.URL(client._httpClient.lastRequest.url);
+      expect(uri.hostname).toEqual('api.edge.us1.twilio.com');
     });
-    var uri = new url.URL(client._httpClient.lastRequest.url);
-    expect(uri.hostname).toEqual('api.edge.region.twilio.com');
-  });
-
-  it('should set the region and edge properly when there is already a region defined', function() {
-    client = new RequestClientMock('ACXXXXXXXX', 'test-password', {
-      uri: 'https://api.region.twilio.com',
+    it('should set the region and edge properly', () => {
+      client.edge = 'edge';
+      client.region = 'region';
+      client.messages.create({
+        body: 'Hello from Node',
+        to: '+XXXXXXXXXX',
+        from: '+XXXXXXXXXX',
+      });
+      var uri = new url.URL(client._httpClient.lastRequest.url);
+      expect(uri.hostname).toEqual('api.edge.region.twilio.com');
     });
-    client.region = 'new_region';
-    client.messages.create({
-      body: 'Hello from Node',
-      to: '+XXXXXXXXXX',
-      from: '+XXXXXXXXXX',
-    });
-    var uri = new url.URL(client._httpClient.lastRequest.url);
-    expect(uri.hostname).toEqual('api.new_region.twilio.com');
-  });
-
-  it('should set the region and edge properly when there is already an edge defined', function() {
-    client = new RequestClientMock('ACXXXXXXXX', 'test-password', {
-      uri: 'https://api.edge.region.twilio.com',
-    });
-    client.edge = 'new_edge';
-    client.region = 'new_region';
-    client.messages.create({
-      body: 'Hello from Node',
-      to: '+XXXXXXXXXX',
-      from: '+XXXXXXXXXX',
-    });
-    var uri = new url.URL(client._httpClient.lastRequest.url);
-    expect(uri.hostname).toEqual('api.new_edge.new_region.twilio.com');
   });
 });
