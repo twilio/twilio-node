cec806f603f4b378c8dd13519153f971a1b1a42f
Jennifer Mah <JENNIFERMAH98@GMAIL.COM>
2021-01-28 10:38:12 -0700

    fix: pagination bug and drop page limit and page limit calculation when listing resources

--- spec/unit/base/Version.spec.js
+++ spec/unit/base/Version.spec.js
@@ -1,7 +1,10 @@
 const Version = require('../../../lib/base/Version');
+const Holodeck = require('../../integration/holodeck')
+const Response = require('../../../lib/http/response');
+const Twilio = require('../../../lib');
 
-describe('fetch method', function() {
-  it('should not throw an exception on 3xx status code', function(done) {
+describe('fetch method', function () {
+  it('should not throw an exception on 3xx status code', function (done) {
     const body = {test: true};
     const version = new Version({
       request: () => Promise.resolve({statusCode: 307, body}),
@@ -14,7 +17,7 @@ describe('fetch method', function() {
     });
   });
 
-  it('should throw an exception if status code >= 400', function(done) {
+  it('should throw an exception if status code >= 400', function (done) {
     const body = {message: 'invalid body'};
     const version = new Version({
       request: () => Promise.resolve({statusCode: 400, body}),
@@ -27,5 +30,32 @@ describe('fetch method', function() {
       done();
     });
   });
+});
+
+describe('Paging limit', function () {
+  const body = {
+    'next_page_uri': '/2010-04-01/Accounts/AC123/Messages.json?Page=1',
+    'messages': [{'body': 'payload0'}, {'body': 'payload1'}],
+    'uri': '/2010-04-01/Accounts/ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/Messages.json?To=%2B123456789&From=%2B987654321&DateSent%3E=2008-01-02&PageSize=2&Page=0'
+  };
+
+  beforeEach(function () {
+    holodeck = new Holodeck();
+    client = new Twilio('ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', 'AUTHTOKEN', {
+      httpClient: holodeck
+    });
+  });
+
+  it('should limit results to one result',
+    function (done) {
+      const messages = [];
+      holodeck.mock(new Response(200, body));
+      client.api.v2010.accounts('ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+        .messages.list({limit: 1}, (m => {
+        messages.push(m);
+        expect(messages.length).toEqual(1);
+      }));
+      done();
+    });
 });
 
