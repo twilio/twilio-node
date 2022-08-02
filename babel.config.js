4bcd7f4d1204ec8942457382382bc7cbed50a44b
Sam Harrison <sharrison@twilio.com>
2022-08-02 11:13:54 -0500

    fix: add tsconfig and enable export assignment

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,7 @@ module.exports = {
       ['@babel/preset-env', {targets: {node: 'current'}}],
       '@babel/preset-typescript',
     ],
+    plugins: [
+      'babel-plugin-replace-ts-export-assignment',
+    ],
   };
-  
