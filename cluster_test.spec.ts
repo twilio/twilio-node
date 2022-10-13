5b4456e22a423f0eb7185489f5e4514f1e8d1ffb
cchua <cchua@twilio.com>
2022-10-13 22:54:55 +0800

    [DI-1412] cluster tests

---
+++ cluster_test.spec.ts
@@ -0,0 +1,163 @@
+'use strict';
+
+import {FlowInstance} from "./lib/rest/studio/v2/flow";
+
+const twilio = require('./lib/index.js');
+const http = require('http');
+const localtunnel = require('localtunnel');
+
+const fromNumber = process.env.TWILIO_FROM_NUMBER;
+const toNumber = process.env.TWILIO_TO_NUMBER;
+const apiKey = process.env.TWILIO_API_KEY;
+const apiSecret = process.env.TWILIO_API_SECRET;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN
+const testClient = twilio(apiKey, apiSecret, {accountSid});
+
+test("Should send a Text", () => {
+  return testClient.messages.create({
+    body: "hello world",
+    to: toNumber,
+    from: fromNumber,
+  }).then(msg => {
+    expect(msg.sid).not.toBeUndefined();
+    expect(msg.to).toBe(toNumber);
+    expect(msg.from).toBe(fromNumber);
+    expect(msg.body).toBe("hello world")
+  })
+})
+
+test("Should list incoming numbers", () => {
+  return testClient.incomingPhoneNumbers
+    .list()
+    .then(incomingPhoneNumbers => {
+      expect(incomingPhoneNumbers).not.toBeNull();
+      expect(incomingPhoneNumbers.length).toBeGreaterThanOrEqual(2);
+    })
+})
+
+test("Should list a incoming number", () => {
+  return testClient.incomingPhoneNumbers
+    .list({limit: 1})
+    .then(incomingPhoneNumbers => {
+      expect(incomingPhoneNumbers).not.toBeUndefined();
+      expect(incomingPhoneNumbers.length).toEqual(1);
+    })
+})
+
+test("Should allow special characters for friendly and identity name", async () => {
+  const friendlyName = 'service|friendly&name'
+  const identityName = 'user|identity&string'
+  const conversation = await testClient.conversations.v1.conversations
+    .create({friendlyName: friendlyName})
+  const participant = await testClient.conversations.v1.conversations(conversation.sid)
+    .participants
+    .create({identity: identityName})
+
+  expect(conversation).not.toBeNull();
+  expect(participant).not.toBeNull();
+  expect(conversation.friendlyName).toBe(friendlyName)
+  expect(participant.identity).toBe(identityName)
+
+  const removeConversation = await testClient.conversations.v1.conversations(conversation.sid)
+    .remove();
+
+  expect(removeConversation).toBeTruthy();
+})
+
+describe('Validating Request', function () {
+  let tunnel;
+  let flowSid;
+  let validationServer;
+  let portNumber = 8080;
+  beforeAll(async () => {
+    validationServer = await http.createServer((req, res) => {
+      let url = req.headers["x-forwarded-proto"] + "://" + req.headers["host"] + req.url
+      let signatureHeader = req.headers["x-twilio-signature"]
+      let body = "";
+      req.on("data", (chunk: string) => {
+        body += chunk;
+      });
+
+      req.on("end", () => {
+        let params = new URLSearchParams(body);
+        let paramObject = Object.fromEntries(params.entries());
+        let requestIsValid = twilio.validateRequest(authToken, signatureHeader, url, paramObject)
+
+        expect(requestIsValid).toBeTruthy();
+      });
+    });
+
+    validationServer.listen(portNumber);
+    tunnel = await localtunnel({port: portNumber});
+  });
+
+  afterAll(() => {
+    tunnel.close();
+    testClient.studio.v2.flows(flowSid).remove();
+    validationServer.close();
+  });
+
+  function CreateStudioFlow(url: string, method: string): Promise<FlowInstance> {
+    return testClient.studio.v2.flows.create({
+      friendlyName: 'Node Cluster Test Flow',
+      status: 'published',
+      definition: {
+        description: 'Studio Flow',
+        states: [
+          {
+            name: 'Trigger',
+            type: 'trigger',
+            transitions: [{
+              "next": "httpRequest",
+              "event": "incomingRequest"
+            }],
+            properties: {}
+          }, {
+            name: "httpRequest",
+            type: "make-http-request",
+            transitions: [],
+            properties: {
+              method: method,
+              content_type: "application/x-www-form-urlencoded;charset=utf-8",
+              url: url
+            }
+          }
+        ],
+        initial_state: 'Trigger',
+        flags: {
+          allow_concurrent_calls: true
+        }
+      }
+    })
+  }
+
+  async function validateRequest(method: string) {
+    let flow = await CreateStudioFlow(tunnel.url, method);
+    flowSid = flow.sid;
+    await testClient.studio.v2.flows(flowSid)
+      .executions
+      .create({to: 'to', from: 'from'});
+    await new Promise(resolve => setTimeout(resolve, 1000))
+  }
+
+  test("Should validate incoming GET request", async () => {
+    await validateRequest('GET');
+
+  })
+
+  test("Should validate incoming POST request", async () => {
+    await validateRequest('POST');
+  })
+});
+
+
+test("Should list available numbers", () => {
+  return testClient.availablePhoneNumbers('US')
+    .tollFree
+    .list({limit: 2})
+    .then(tollFree => {
+      expect(tollFree).not.toBeNull();
+      expect(tollFree.length).toEqual(2);
+    });
+})
