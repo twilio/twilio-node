aed7fda3da295a5784f42677891e9234d0fd41a7
childish-sambino <sharrison@twilio.com>
2022-12-06 09:39:25 -0600

    fix: validate non-string path params (#835)

--- lib/base/utility.ts
+++ lib/base/utility.ts
@@ -1,11 +1,14 @@
+const INVALID_PATH_PARAM_CHARS = ["/", "?"];
+
 export const trim = (str, c = "\\s") =>
   str.replace(new RegExp(`^([${c}]*)(.*?)([${c}]*)$`), "$2");
 
-export function isValidPathParam(param: string): boolean {
-  return (
-    param !== null &&
-    param !== undefined &&
-    !param.includes("/") &&
-    !param.includes("?")
+export function isValidPathParam(param: any): boolean {
+  if (param === null || param === undefined) return false;
+
+  const paramString = param.toString();
+
+  return INVALID_PATH_PARAM_CHARS.every(
+    (invalidChar) => !paramString.includes(invalidChar)
   );
 }
