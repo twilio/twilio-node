1f1ae519702117c5dcc6b3c4fc0e774ca9cc1b3e
mariomui <yi.kan.mui@gmail.com>
2022-12-29 12:57:50 -0800

    chore: fix SMS messaging parse error

--- lib/base/RestException.ts
+++ lib/base/RestException.ts
@@ -9,15 +9,39 @@ export default class RestException extends Error {
 
   constructor(response) {
     super("[HTTP " + response.statusCode + "] Failed to execute request");
+    const [body, bodyParseError] = parseResponseBody(response.body);
 
-    const body =
-      typeof response.body === "string"
-        ? JSON.parse(response.body)
-        : response.body;
     this.status = response.statusCode;
-    this.message = body.message;
-    this.code = body.code;
-    this.moreInfo = body.more_info; /* jshint ignore:line */
-    this.details = body.details;
+    if (body) {
+      this.message = body.message;
+      this.code = body.code;
+      this.moreInfo = body.more_info; /* jshint ignore:line */
+      this.details = body.details;
+    }
+    if (body === null) {
+      this.message = "Failed to parse response body";
+      this.code = 12100;
+      this.moreInfo = "https://www.twilio.com/docs/api/errors/12100";
+      this.details = { error: bodyParseError };
+    }
   }
+}
+/**
+ * @param {serialized object | object} response_body
+ * @desc returns a tuple containing the reponse_body in object form or null
+ * and an json parse error.
+ * @returns {[responseBody: object | null , err]} err is the json parse error
+ */
+function parseResponseBody(response_body) {
+  let body = {};
+  let err = null;
+  try {
+    body = JSON.parse(response_body);
+  } catch (catchError) {
+    const isObject =
+      Object.prototype.toString.call(response_body) === "[object Object]";
+    body = isObject ? response_body : null;
+    err = catchError;
+  }
+  return [body, err];
 }
--- spec/unit/base/RestException.spec.ts
+++ spec/unit/base/RestException.spec.ts
@@ -6,6 +6,7 @@ describe("exception gets created from string", function () {
       statusCode: 200,
       body: '{"message":"test", "code":81022,"more_info": "https://www.twilio.com/docs/errors/81022"}',
     };
+
     const exception = new RestException(response);
     expect(exception.status).toEqual(200);
     expect(exception.message).toEqual("test");
@@ -13,6 +14,20 @@ describe("exception gets created from string", function () {
     expect(exception.moreInfo).toEqual(
       "https://www.twilio.com/docs/errors/81022"
     );
+  });
+  it("should test serialize from improper json string", function () {
+    const response = {
+      statusCode: 200,
+      body: '{message":test", "code:81022,"more_info": "https://www.twilio.com/docs/errors/12100"}',
+    };
+    const exception = new RestException(response);
+    expect(exception.status).toEqual(200);
+    expect(exception.message).toEqual("Failed to parse response body");
+    expect(exception.code).toEqual(12100);
+    expect(exception.moreInfo).toEqual(
+      "https://www.twilio.com/docs/api/errors/12100"
+    );
+    expect(exception.details["error"] instanceof Error).toBe(true);
   });
 });
 
