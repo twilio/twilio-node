917f362c4e48d4c9f9abc39f3e501c31153e33ff
Sam Harrison <sharrison@twilio.com>
2023-01-04 13:31:48 -0600

    fix instance iteration

--- lib/base/Version.ts
+++ lib/base/Version.ts
@@ -266,7 +266,7 @@ export default class Version {
         return;
       }
       promise.then((page) => {
-        Object.keys(page.instances).forEach(function (instance: any) {
+        page.instances.forEach(function (instance: any) {
           if (
             done ||
             (typeof params.limit !== "undefined" &&
