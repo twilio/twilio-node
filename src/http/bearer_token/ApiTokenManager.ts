40aab24ccd6e691fe1e7b711f0b0788e111740d3
Shubham Tiwari <stiwari@twilio.com>
2024-11-14 12:31:54 +0530

    chore: add token managers

---
+++ src/http/bearer_token/ApiTokenManager.ts
@@ -0,0 +1,25 @@
+import TokenManager from "./TokenManager";
+import { TokenListInstance, TokenListInstanceCreateOptions } from "../../rest/previewIam/v1/token";
+import PreviewIamBase from "../../rest/PreviewIamBase";
+import V1 from "../../rest/previewIam/V1";
+import NoAuthCredentialProvider from "../../credential_provider/NoAuthCredentialProvider";
+import { Client } from "../../base/BaseTwilio";
+
+export default class ApiTokenManager implements TokenManager {
+    private params: TokenListInstanceCreateOptions;
+
+    constructor(params: TokenListInstanceCreateOptions) {
+        this.params = params;
+    }
+
+    async fetchToken(): Promise<string> {
+        const noAuthCredentialProvider = new NoAuthCredentialProvider();
+        const client = new Client();
+        client.setCredentialProvider(noAuthCredentialProvider);
+
+        const tokenListInstance = TokenListInstance(new V1(new PreviewIamBase(client)));
+        const token = await tokenListInstance.create(this.params);
+        return token.accessToken;
+    }
+
+}
